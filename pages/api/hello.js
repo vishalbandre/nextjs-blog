28dcfad3357aaaa1094f089dfec0162263ef0a1d
vish <vshlbndr@gmail.com>
2021-10-10 19:49:58 +0530

    Minor updates

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-    res.status(200).json({ text: 'Welcome', title: 'Factober Media' })
+    res.status(200).json({ text: 'Welcome', title: 'Next.js API' })
 }
