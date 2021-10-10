c91450c8d666a88aa9b8b2ab1ba0f13332c6730a
vish <vshlbndr@gmail.com>
2021-10-10 19:55:14 +0530

    Minor updates

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Next.js App'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Cool Next.js'
 
 export default function Layout({ children, home }) {
     return (
